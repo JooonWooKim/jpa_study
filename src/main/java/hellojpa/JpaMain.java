4024111dd67efb2fc2ffef5fdf81cfa748dd2179
Joonwoo Kim <kkn8010@gmail.com>
2022-08-12 17:09:14 +0900

    study: entity 1차 캐시 학습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -26,17 +26,22 @@ public class JpaMain {
 //            2.회원 수정
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("HelloJPA");
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+            /////////////////비영속, 아무런 등록을 해주지 않은 상태
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    //pagenation, 첫번째부터
-                    .setFirstResult(1)
-                    //8번까지
-                    .setMaxResults(8)
-                    .getResultList();
+            //영속
+            System.out.println("BEFORE");
+            em.persist(member);
+            System.out.println("after");
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+
+            System.out.println("findMember.getId() = " + findMember1.getId());
+            System.out.println("findMember.getId() = " + findMember1.getName());
+            
 
             tx.commit();
         }catch(Exception e){
